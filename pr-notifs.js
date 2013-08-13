577475a881cf3eaf2fd48d44faa2efd60353fe5b
Ryan Roemer <ryan@formidablelabs.com>
2013-08-13 13:25:02 -0700

    Add iniparser.

--- pr-notifs.js
+++ pr-notifs.js
@@ -3,15 +3,22 @@
 /**
  * Pull request notifications.
  */
-var pkg = require("./package.json"),
-  program = require("commander");
+var iniparser = require("iniparser"),
+  pkg = require("./package.json"),
+  program = require("commander"),
+  HOME_PATH = process.env.HOME,
+  GIT_CONFIG_PATH = [HOME_PATH, ".gitconfig"].join("/"),
+  GIT_CONFIG;
 
 // Parse command line arguments.
 program
   .version(pkg.version)
   .parse(process.argv);
 
+// Parse gitconfig file.
+GIT_CONFIG = iniparser.parseSync(GIT_CONFIG_PATH);
+
 // Main.
 if (require.main === module) {
-  console.log("TODO HERE", program);
+  console.log("TODO HERE", GIT_CONFIG);
 }
